22aa2ec2162e449e86f73d1f8fa0f940bd955100
Tristan Colgate <tcolgate@gmail.com>
2016-06-19 17:42:21 +0100

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2016 Tristan Colgate-McFarlane
+//
+// This file is part of hugot.
+//
+// hugot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// hugot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with hugot.  If not, see <http://www.gnu.org/licenses/>.
+
+// Package hugot provides a simple interface for building extensible
+// chat bots in an idiomatic go style. It is heavily influenced by
+// net/http
+package hugot
