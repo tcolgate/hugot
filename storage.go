69b6e4a914f98e9b097a7a1aaf92175f7b70454a
Tristan Colgate <tristan@qubit.com>
2016-12-06 15:44:37 +0000

    Rework Storer interface to match libkv

--- storage.go
+++ storage.go
@@ -1,32 +1,55 @@
 package hugot
 
-// Storer is an interface to external key/value storage
+import (
+	"strings"
+
+	"github.com/docker/libkv/store"
+)
+
 type Storer interface {
-	Get(key []byte) ([]byte, bool, error)
-	Set(key []byte, value []byte) error
-	Unset(key []byte) error
+	Get(key string) (*store.KVPair, error)
+	Put(key string, value []byte, options *store.WriteOptions) error
+	Delete(key string) error
 }
 
 type prefixStore struct {
-	pfx  []byte
+	pfx  string
 	base Storer
 }
 
-func (p prefixStore) Get(key []byte) ([]byte, bool, error) {
-	return p.base.Get(append(p.pfx, key...))
+func (p prefixStore) Get(key string) (*store.KVPair, error) {
+	res, err := p.base.Get(prepend(key, p.pfx))
+	if res != nil {
+		res.Key = strip(res.Key, p.pfx)
+	}
+	return res, err
 }
 
-func (p prefixStore) Set(key []byte, value []byte) error {
-	return p.base.Set(append(p.pfx, key...), value)
+func (p prefixStore) Put(key string, value []byte, options *store.WriteOptions) error {
+	return p.base.Put(prepend(key, p.pfx), value, options)
 }
 
-func (p prefixStore) Unset(key []byte) error {
-	return p.base.Unset(append(p.pfx, key...))
+func (p prefixStore) Delete(key string) error {
+	return p.base.Delete(prepend(key, p.pfx))
 }
 
-func newPrefixedStore(pfx []byte, s Storer) prefixStore {
+func newPrefixedStore(s Storer, pfx string) prefixStore {
 	return prefixStore{
-		pfx:  append(pfx, []byte("#")...),
+		pfx:  pfx,
 		base: s,
 	}
+}
+
+func prepend(s, pfx string) string {
+	ks := store.SplitKey(s)
+	ks = append([]string{pfx}, ks...)
+	return "/" + strings.Join(ks, "/")
+}
+
+func strip(s, pfx string) string {
+	ks := store.SplitKey(s)
+	if len(ks) > 1 && ks[0] == pfx {
+		ks = ks[1:]
+	}
+	return "/" + strings.Join(ks, "/")
 }
--- storage_test.go
+++ storage_test.go
@@ -1,1 +1,41 @@
 package hugot
+
+import (
+	"testing"
+
+	"github.com/docker/libkv/store"
+	"github.com/docker/libkv/store/mock"
+)
+
+func newMock() (store.Store, error) {
+	return mock.New([]string{}, nil)
+}
+
+func TestPrefixStore_Put(t *testing.T) {
+	s, _ := newMock()
+
+	s.(*mock.Mock).On("Put", "/myhandler/a key", []byte("value"), (*store.WriteOptions)(nil)).Return(nil)
+	ps := newPrefixedStore(s, "myhandler")
+
+	ps.Put("a key", []byte("value"), nil)
+
+	s.(*mock.Mock).AssertExpectations(t)
+}
+
+func TestPrefixStore_Get(t *testing.T) {
+	s, _ := newMock()
+
+	s.(*mock.Mock).On("Get", "/myhandler/a key").Return(&store.KVPair{Key: "/myhandler/a key", Value: []byte("value")}, nil)
+
+	ps := newPrefixedStore(s, "myhandler")
+
+	v, _ := ps.Get("a key")
+
+	s.(*mock.Mock).AssertExpectations(t)
+	if v.Key != "a key" {
+		t.Fatal("wrong key value")
+	}
+}
+
+func TestPrefixedStore_Get(t *testing.T) {
+}
