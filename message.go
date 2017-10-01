fc16a99669ba966cdcaa5b483736ed96a3835dc2
Tristan Colgate <tcolgate@gmail.com>
2017-10-01 13:19:24 +0100

    core: Add utilities for building attachments from templates

--- message.go
+++ message.go
@@ -18,8 +18,12 @@
 package hugot
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"text/template"
 
+	"github.com/golang/glog"
 	"github.com/nlopes/slack"
 	"github.com/tcolgate/hugot/storage"
 )
@@ -72,4 +76,83 @@ func (m *Message) Reply(txt string) *Message {
 // and the from/to fields switched.
 func (m *Message) Replyf(s string, is ...interface{}) *Message {
 	return m.Reply(fmt.Sprintf(s, is...))
+}
+
+func stringFromTemplate(tmpls *template.Template, name string, data interface{}) string {
+	if tmpls.Lookup(name) != nil {
+		out := bytes.Buffer{}
+		err := tmpls.ExecuteTemplate(&out, name, data)
+		if err != nil {
+			glog.Infof("error expanding title template, ", err.Error())
+			return ""
+		}
+		return out.String()
+	}
+	return ""
+}
+
+// AttachmentFieldFromTemplates builds an attachment field from a set of templates
+// templates can include "field_title","field_value", and "field_short". "field_short"
+// should expand to "true" or "false".
+func AttachmentFieldFromTemplates(tmpls *template.Template, data interface{}) (slack.AttachmentField, error) {
+	fieldTitle := stringFromTemplate(tmpls, "field_title", data)
+	fieldValue := stringFromTemplate(tmpls, "field_title", data)
+	short := false
+	fieldShortStr := stringFromTemplate(tmpls, "field_short", data)
+	if fieldShortStr == "true" {
+		short = true
+	}
+
+	return slack.AttachmentField{
+		Title: fieldTitle,
+		Value: fieldValue,
+		Short: short,
+	}, nil
+}
+
+// AttachmentFromTemplates builds an attachment from aset of templates. Template can include
+// the following individual template items: title, title_link, color,
+// author_name, author_link, author_icon, image_url,
+// thumb_url, pretext, text, fallback, fields_json.
+// fields_json will be parsed as json, and any fields found will be appended to those in the fields
+// arguments
+func AttachmentFromTemplates(tmpls *template.Template, data interface{}, fields ...slack.AttachmentField) (Attachment, error) {
+	title := stringFromTemplate(tmpls, "title", data)
+	titleLink := stringFromTemplate(tmpls, "title_link", data)
+	color := stringFromTemplate(tmpls, "color", data)
+	authorName := stringFromTemplate(tmpls, "author_name", data)
+	authorLink := stringFromTemplate(tmpls, "author_link", data)
+	authorIcon := stringFromTemplate(tmpls, "author_icon", data)
+	imageURL := stringFromTemplate(tmpls, "image_url", data)
+	thumbURL := stringFromTemplate(tmpls, "thumb_url", data)
+	pretext := stringFromTemplate(tmpls, "pretext", data)
+	text := stringFromTemplate(tmpls, "text", data)
+	fallback := stringFromTemplate(tmpls, "fallback", data)
+
+	fieldsJSON := stringFromTemplate(tmpls, "fields_json", data)
+	var jfields []slack.AttachmentField
+	if fieldsJSON != "" {
+		err := json.Unmarshal([]byte(fieldsJSON), jfields)
+		if err != nil {
+			glog.Infof("error parsing fields as json, ", err.Error())
+		}
+	}
+
+	fields = append(fields, jfields...)
+
+	return Attachment{
+		Title:      title,
+		TitleLink:  titleLink,
+		AuthorName: authorName,
+		AuthorIcon: authorIcon,
+		AuthorLink: authorLink,
+		ThumbURL:   thumbURL,
+		ImageURL:   imageURL,
+		Color:      color,
+		Pretext:    pretext,
+		Text:       text,
+		Fallback:   fallback,
+		Fields:     fields,
+		MarkdownIn: []string{"pretext", "text", "fields"},
+	}, nil
 }
